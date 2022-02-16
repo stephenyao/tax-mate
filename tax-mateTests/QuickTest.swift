9c6c0840828a3f85b90e3c02589f841f8a2046e4
Stephen Yao <stephen.yao@rea-group.com>
2022-02-16 23:59:09 +1100

    Added a quick / nimble test

---
+++ tax-mateTests/QuickTest.swift
@@ -0,0 +1,21 @@
+////
+////  QuickTest.swift
+////  tax-mateTests
+////
+////  Created by Stephen Yao on 16/2/22.
+////
+//
+//import Quick
+//import Nimble
+//
+//@testable import tax_mate
+//
+//final class QuickTest: QuickSpec {
+//  override func spec() {
+//    describe("Test") {
+//      it("should pass") {
+//        expect(1 == 1).to(beTrue())
+//      }
+//    }
+//  }
+//}
